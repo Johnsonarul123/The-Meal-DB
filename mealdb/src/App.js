6dc5a13427d9e6d4355a915bedb02c13e2c6ec47
[Arul Johnson] <[aruljohnson9411@gmail.com]>
2023-05-15 22:20:38 +0530

    updated the list and basic components

--- mealdb/src/App.js
+++ mealdb/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+// make a search bar 
+// fetch the api 
+//display api dta in component 
+
+
 import './App.css';
+import React,{useEffect, useState} from 'react';
+import SearchBar from './components/SearchBar';
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+   
+  // search for the recipe
+    const searchRecipes = async () =>{
+      setIsLoading(true);
+      const url = searchApi + query
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setRecipes(data.meal);
+      setIsLoading(false);
+    }
+
+    useEffect(()=>{
+      searchRecipes();
+    },[]);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      searchRecipes();
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> MealDb App</h1>
+        <SearchBar
+          isLoading={isLoading}
+          query={query}
+          setQuery={setQuery}
+          handleSubmit={handleSubmit}
+        />
+
       </header>
     </div>
   );
